cffc53f69348b286e3cfb789eaf201aac4947141
Wiaz24 <wiazhub@gmail.com>
2023-10-17 22:35:10 +0200

    Updated adminpanel

--- 5.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('cc2022.csv')
-
-plt.plot(df['datetime'], df['Actual Total Load'])
-
-plt.show()
--- combine_datetime.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('2022.csv')
-
-df['datetime'] = df.apply(lambda row: pd.to_datetime(row['Date'] + ' ' + row['Hour'], format='%Y-%m-%d %H:%M:%S'), axis=1)
-
-df = df.drop(['Date', 'Hour'], axis=1)
-
-df.to_csv('c2022.csv', index=False)
